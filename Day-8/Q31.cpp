f88b71540dd7122e42f41093fc3937488a51e696
Vanswock <vedansh0202@gmail.com>
2026-06-08 15:34:11 +0530

    Day-8 assignment is uploaded

---
+++ Day-8/Q31.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main()
+{
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            std::cout<<char('A'+j);
+        }
+        std::cout<<"\n";
+    }
+}
