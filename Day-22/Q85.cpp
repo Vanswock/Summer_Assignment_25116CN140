578fe0a5a6f5562596ccee9937b52007fe9db77b
Vanswock <vedansh0202@gmail.com>
2026-06-22 13:28:39 +0530

    Day-22 assignment is uploaded

---
+++ Day-22/Q85.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char str[100];
+    int length = 0;
+    bool palindrome = true;
+
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+
+    // Find length
+    while (str[length] != '\0')
+    {
+        length++;
+    }
+
+    // Check palindrome
+    for (int i = 0; i < length / 2; i++)
+    {
+        if (str[i] != str[length - 1 - i])
+        {
+            palindrome = false;
+            break;
+        }
+    }
+
+    if (palindrome)
+        cout << "String is Palindrome";
+    else
+        cout << "String is Not Palindrome";
+
+    return 0;
+}
